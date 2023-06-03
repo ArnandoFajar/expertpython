ad2dc501853dd0445e6115e221059f133ada0831
arnandofajar <arnandofajar@gmail.com>
2023-06-03 16:53:56 +0700

    loop tuple

--- tuple.py
+++ tuple.py
@@ -49,3 +49,69 @@ print(thistuple[2:5])
 thistuple = ("apple", "banana", "cherry")
 if "apple" in thistuple:
     print("Yes, 'apple' is in the fruits tuple")
+
+# Change Tuple Values
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+
+# Add Items
+# Since tuples are immutable, they do not have a built-in append() method, but there are other ways to add items to a tuple.
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
+
+# Remove Items
+# Tuples are unchangeable, so you cannot remove items from it, but you can use the same workaround as we used for changing and adding tuple items:
+# Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+print(thistuple)
+
+# Unpacking a Tuple
+# When we create a tuple, we normally assign values to it. This is called "packing" a tuple:
+# Packing a tuple:
+fruits = ("apple", "banana", "cherry")
+# But, in Python, we are also allowed to extract the values back into variables. This is called "unpacking":
+# Unpacking a tuple:
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# Using Asterisk*
+# If the number of variables is less than the number of values, you can add an * to the variable name and the values will be assigned to the variable as a list:
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# If the asterisk is added to another variable name than the last, Python will assign values to the variable until the number of values left matches the number of variables left.
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
+
+# Loop Through a Tuple
+# You can loop through the tuple items by using a for loop.
+thistuple = ("apple", "banana", "cherry")
+for i in range(0, 2):
+    print(thistuple[i])
+
+print(range(len(thistuple)))
